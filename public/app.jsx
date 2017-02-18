d44d8b2d0d02f4d8a53f0526c0144f853c0def50
Michael Campos <camposmj89@gmail.com>
2017-02-18 06:29:41 +0000

    Added state changes

--- public/app.jsx
+++ public/app.jsx
@@ -1,3 +1,53 @@
+var Greeter = React.createClass({
+	getDefaultProps: function() {
+		return {
+			name: 'React',
+			message: 'This is my first tutorial for React'
+		};
+	},
+
+	getInitialState: function() {
+		return {
+			name: this.props.name
+		}
+	},
+
+	onButtonClick: function(e) {
+		e.preventDefault();
+
+		var nameRef = this.refs.name;
+		var name = nameRef.value;
+		nameRef.value = "";
+
+		if (typeof name === "string" && name.length > 0) {
+			this.setState({
+				name: name
+			})
+		};
+	},
+
+	render: function() {
+		var name = this.state.name;
+		var message = this.props.message
+
+		return (
+			<div>
+				<h1>Hello {name}!</h1>
+				<p>{message}</p>
+
+				<form onSubmit={this.onButtonClick}>
+					<input type="text" ref="name"/>
+					<button>Set Name</button>
+				</form>
+			</div>
+		);
+	}
+});
+
+var firstName = "Michael";
+var message = "It's you a Boss waits for";
+
 ReactDOM.render(
-	<h1>Hello React</h1>, document.getElementById('app')
-)
+	<Greeter name={firstName}/>, 
+	document.getElementById('app')
+);
